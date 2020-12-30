eaba750006509ca1dc57f48b0da4314ad65fbd97
Andy Paddock <appaddock@gmail.com>
2020-12-30 13:49:39 +0000

    minor Git test

--- single-property.php
+++ single-property.php
@@ -9,7 +9,7 @@ get_header();?>
 <?php get_template_part('template-parts/horizontal-info');?>
 <div class="other-properties mt8 mb8">
     <div class="container container__full">
-        <h4 class="heading heading__4">Other Chalets</h4>
+        <h4 class="heading heading__4">Other Chalets </h4>
     </div>
     <div class="container mt3">
         <?php get_template_part('template-parts/home-property-feed-av');?>
